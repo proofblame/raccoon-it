6814f8fa0f4eb3a4aa9a857fee249a081822243c
Shyou_92 <vitalex92@yandex.ru>
2022-01-17 19:24:11 +0300

    initial commit 2

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,13 +1,17 @@
 import about from './About.module.scss';
 
 import Container from '../../shared/UI/Container/Container';
+import Button from '../../shared/UI/Button/Button';
 import H2 from '../../shared/UI/H2/H2';
 
 const About = () => {
   return (
     <section className={about.section}>
       <Container>
-        <H2></H2>
+        <div className={about.wrapper}>
+          <H2>О нас</H2>
+        </div>
+        <Button>Оставить заявку</Button>
       </Container>
     </section>
   );
