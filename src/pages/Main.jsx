d37386c1fc5ac03cd22c1916bb3a5770c341f647
Shyou_92 <vitalex92@yandex.ru>
2022-01-18 19:23:01 +0300

    projects layout

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import About from './screens/About/About';
 import Tenets from './screens/Tenets/Tenets';
 import main from './Main.module.scss';
+import Projects from './screens/Projects/Projects';
 
 const Main = () => {
   return (
     <div className={main.wrapper}>
       <About />
       <Tenets />
+      <Projects />
     </div>
   );
 };
---
+++ src/pages/screens/Projects/Projects.jsx
@@ -0,0 +1,57 @@
+import projects from './Projects.module.scss';
+
+import H2 from '../../../shared/UI/H2/H2';
+import Container from '../../../shared/UI/Container/Container';
+import IMG from '../../../shared/UI/IMG/IMG';
+import H3 from '../../../shared/UI/H3/H3';
+import Button from '../../../shared/UI/Button/Button';
+
+import Lawyers from './images/Lawyers.svg';
+import Psy from './images/Psy.svg';
+import CRMCMS from './images/CRMCMS.svg';
+
+const Projects = () => {
+  return (
+    <section className={projects.section}>
+      <Container>
+        <div className={projects.wrapper}>
+          <H2 className={projects.header}>Наши проекты</H2>
+          <div className={projects.listWrapper}>
+            <ul className={projects.list}>
+              <li className={projects.listItem}>
+                <IMG
+                  src={Lawyers}
+                  alt='Юристы в Омске'
+                  className={projects.listItemImage}
+                />
+                <H3 className={projects.listHeader}>Юристы в Омске</H3>
+              </li>
+              <li className={projects.listItem}>
+                <IMG
+                  src={Psy}
+                  alt='Онлайн консультации'
+                  className={projects.listItemImage}
+                />
+                <H3 className={projects.listHeader}>Онлайн консультации</H3>
+              </li>
+              <li className={projects.listItem}>
+                <IMG
+                  src={CRMCMS}
+                  alt='СRM + CMS'
+                  className={projects.listItemImage}
+                />
+                <H3 className={projects.listHeader}>СRM + CMS</H3>
+              </li>
+            </ul>
+          </div>
+          <div className={projects.container}>
+            <Button>Оставить заявку</Button>
+            <p className={projects.subscription}>Стань следующим!</p>
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default Projects;
