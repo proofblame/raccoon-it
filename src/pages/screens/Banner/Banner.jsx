ec4e13194c3b52c308d579edd9f7cb9882a5ae7a
Shyou_92 <vitalex92@yandex.ru>
2022-01-26 10:00:01 +0300

    banner

--- src/pages/screens/Banner/Banner.jsx
+++ src/pages/screens/Banner/Banner.jsx
@@ -2,8 +2,13 @@ import styles from './Banner.module.scss';
 import H1 from '../../../shared/UI/H1/H1';
 import Button from '../../../shared/UI/Button/Button';
 import logo from './images/logo.svg';
-import imgBlock from './images/img-block.svg';
-// import Hand from './images/Hand.svg';
+import Hand from './images/Hand.svg';
+import Fingers from './images/Fingers.svg';
+import Mobile from './images/Mobile.svg';
+import Mobile1Part from './images/Mobile1part.svg';
+import Mobile2Part from './images/Mobile2part.svg';
+import Mobile3Part from './images/Mobile3part.svg';
+import Mobile4Part from './images/Mobile4part.svg';
 import Container from '../../../shared/UI/Container/Container';
 import Navbar from './Navbar/Navbar';
 
@@ -39,12 +44,36 @@ const Banner = ({ isOpened, handleOpened, handleCloseByClickOnOverlay }) => {
               </p>
               <Button>Оставить заявку</Button>
             </div>
+
             <div className={styles.imgWrapper}>
               <img
-                src={imgBlock}
+                src={Hand}
                 alt='synthetic hand'
                 className={styles.imgBlock}
               />
+              <div className={styles.mobileBlock}>
+                <img src={Mobile} alt='mobile' className={styles.mobile} />
+                <img
+                  src={Mobile1Part}
+                  alt='Mobile1Part'
+                  className={styles.mobile1Part}
+                />
+                <img
+                  src={Mobile2Part}
+                  alt='Mobile2Part'
+                  className={styles.mobile2Part}
+                />
+                <img
+                  src={Mobile3Part}
+                  alt='Mobile3Part'
+                  className={styles.mobile3Part}
+                />
+                <img
+                  src={Mobile4Part}
+                  alt='Mobile4Part'
+                  className={styles.mobile4Part}
+                />
+              </div>
             </div>
             <div className={styles.pages}></div>
           </div>
