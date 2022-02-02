9cfffda8e10d4c79b4261c72fba661c5cfa872e7
Тимур Марокко <69273257+proofblame@users.noreply.github.com>
2022-02-02 22:05:12 +0300

    with scheme

--- src/components/main/work-scheme/work-scheme.jsx
+++ src/components/main/work-scheme/work-scheme.jsx
@@ -1,17 +1,26 @@
 import { Link } from 'react-scroll';
 
-import styles from './work-scheme.module.scss'
-import H2 from '../../../shared/UI/h2/h2'
-import Button from '../../../shared/UI/button/button'
+import styles from './work-scheme.module.scss';
+import H2 from '../../../shared/UI/h2/h2';
+import Button from '../../../shared/UI/button/button';
 
-import schemeDesktop from '../../../shared/images/scheme-desktop.svg'
-import schemeMobile from '../../../shared/images/scheme-mobile.svg'
+import schemeDesktop from '../../../shared/images/scheme-desktop.svg';
+import schemeMobile from '../../../shared/images/scheme-mobile.svg';
+import backlogProject from '../../../shared/images/mobile-scheme/backlog-project.svg';
+import backlogSprint from '../../../shared/images/mobile-scheme/backlog-sprint.svg';
+import arrow from '../../../shared/images/mobile-scheme/arrow.svg';
+import daily from '../../../shared/images/mobile-scheme/daily.svg';
+import rectangle from '../../../shared/images/mobile-scheme/rectangle.svg';
 
 const WorkScheme = () => {
   return (
     <section className={styles.workScheme} id='workflow'>
       <H2 className={styles.title}>Как мы работаем?</H2>
-      <p className={styles.subtitle}>Мы изучаем особенности ваших клиентов и подбираем лучшее решение для вашего бизнеса. Работаем по гибкой методологии Agile, что позволяет быстро менять продукт под требования рынка.</p>
+      <p className={styles.subtitle}>
+        Мы изучаем особенности ваших клиентов и подбираем лучшее решение для
+        вашего бизнеса. Работаем по гибкой методологии Agile, что позволяет
+        быстро менять продукт под требования рынка.
+      </p>
       <ul className={styles.list}>
         <li className={styles.item}>
           <p className={styles.itemNumber}>01</p>
@@ -31,8 +40,50 @@ const WorkScheme = () => {
         </li>
       </ul>
       <div className={styles.scheme}>
-        <img className={styles.schemeDesktop} src={schemeDesktop} alt="Как мы работаем" />
-        <img className={styles.schemeMobile} src={schemeMobile} alt="Как мы работаем" />
+        <img
+          className={styles.schemeDesktop}
+          src={schemeDesktop}
+          alt='Как мы работаем'
+        />
+        {/* <img className={styles.schemeMobile} src={schemeMobile} alt="Как мы работаем" /> */}
+        <div className={styles.schemeMobile}>
+          <div className={styles.schemeBlock}>
+            <img
+              className={styles.schemeRectangle}
+              src={backlogProject}
+              alt='бэклог проекта'
+            />
+            <p className={styles.schemeTitle}>бэклог проекта</p>
+          </div>
+          <img className={styles.schemeArrow} src={arrow} alt='arrow' />
+          <div className={styles.schemeBlock}>
+            <img className={styles.schemeRectangle} src={backlogSprint} alt='Бэклог спринта' />
+            <p className={styles.schemeTitle}>Бэклог спринта</p>
+          </div>
+          <img className={styles.schemeArrow} src={arrow} alt='arrow' />
+          <div className={styles.schemeBlock}>
+            <img className={styles.schemeIMG} src={daily} alt='daily' />
+            <p className={styles.schemeTitle}>Спринт</p>
+            <ul className={styles.schemeList}>
+              <li className={styles.schemeItem}>дизайн</li>
+              <li className={styles.schemeItem}>Разработка</li>
+              <li className={styles.schemeItem}>Тестирование</li>
+              <li className={styles.schemeItem}>Наладка</li>
+              <li className={styles.schemeItem}>поддержка</li>
+            </ul>
+          </div>
+          <img className={styles.schemeArrow} src={arrow} alt='arrow' />
+          <div className={styles.schemeBlock}>
+            <img
+              className={styles.schemeRectangle}
+              src={rectangle}
+              alt='Работающее ПО с новыми возможностями'
+            />
+            <p className={styles.schemeTitle}>
+              Работающее По с новыми возможностями
+            </p>
+          </div>
+        </div>
       </div>
 
       <div className={styles.footer}>
@@ -40,9 +91,8 @@ const WorkScheme = () => {
           <Button>Оставить заявку</Button>
         </Link>
       </div>
-
     </section>
-  )
+  );
 };
 
 export default WorkScheme;
